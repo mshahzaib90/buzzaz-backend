d5db2bdfb1de596fe29fd10b88617fd8e4718909
mshahzaib90 <md.shahzaib@gmail.com>
2026-01-13 16:29:03 +0500

    fix: force include favicon.svg and unignore public folder

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 
+console.log('Buzzaz Frontend v1.1.0 loaded - Dynamic Titles & Favicon');
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
